5f9c89c288dd722b6c6b15c867e784a47bd88709
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-08-29 16:59:31 +0200

    @PathVariable & @RequestParam

--- src/main/java/de/cegos/SchulungSpring/rest/HelloController.java
+++ src/main/java/de/cegos/SchulungSpring/rest/HelloController.java
@@ -1,12 +1,20 @@
 package de.cegos.SchulungSpring.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hallöchen";
+
+    @GetMapping("hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hallöchen " + name;
+    }
+
+    @GetMapping("bye")
+    public String sayGoodbye(@RequestParam String name) {
+        return "Goodbye " + name;
     }
 }
