4cbe09e83abe461f8ef07bad751f07c7b03f930b
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-08-30 16:46:02 +0200

    @RequestParam with changed Name and default added

--- src/main/java/de/cegos/SchulungSpring/rest/controller/StudentController.java
+++ src/main/java/de/cegos/SchulungSpring/rest/controller/StudentController.java
@@ -5,6 +5,7 @@ import de.cegos.SchulungSpring.rest.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,11 @@ public class StudentController {
     @GetMapping
     public List<Student> findAll() {
         return studentService.findAll();
+    }
+
+    @GetMapping("/create")
+    public List<Student> findContainingString(
+            @RequestParam(name = "n", defaultValue = "5") Integer amount) {
+        return studentService.saveRandomStudents(amount);
     }
 }
