78d8e3ef97ffa9040cea2bcce240ada27e68ee60
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-08-31 11:55:39 +0200

    Start Job by ourself

--- src/main/java/de/cegos/SchulungSpring/batch/JobStarter.java
+++ src/main/java/de/cegos/SchulungSpring/batch/JobStarter.java
@@ -1,13 +1,31 @@
 package de.cegos.SchulungSpring.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobStarter implements CommandLineRunner {
 
+    JobLauncher jobLauncher;
+    Job job;
+
+    public JobStarter(JobLauncher jobLauncher, Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        jobLauncher.run(job, jobParameters());
+    }
 
+    private JobParameters jobParameters() {
+        return new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis())
+                .toJobParameters();
     }
 }
