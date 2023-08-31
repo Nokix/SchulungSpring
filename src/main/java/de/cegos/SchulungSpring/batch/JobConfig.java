9903373c6e878b6807be6bee834238cf9959e6b1
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-08-31 13:24:52 +0200

    Reader Processor Writer Job

--- src/main/java/de/cegos/SchulungSpring/batch/JobConfig.java
+++ src/main/java/de/cegos/SchulungSpring/batch/JobConfig.java
@@ -3,17 +3,19 @@ package de.cegos.SchulungSpring.batch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -37,4 +39,26 @@ public class JobConfig {
                 .start(step)
                 .build();
     }
+
+    @Bean
+    @Primary
+    Job getSingleStepJob(Step step) {
+        return new JobBuilder("My Job", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    Step getListSouterStep() {
+        List<String> input = List.of("a", "b", "c", "d", "e", "f", "g");
+
+        return new StepBuilder("Sout List on Console", jobRepository)
+                .<String, String>chunk(3, transactionManager)
+                .reader(new ListItemReader<>(input))
+                .processor(String::toUpperCase)
+                .writer(chunk -> chunk.forEach(System.out::println))
+                .build();
+    }
+
+
 }
